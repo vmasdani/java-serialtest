fe09ed03cda37cf2b04e019c3720d3085c91b0b5
valiangspe <valian@gspe.co.id>
2020-09-10 17:41:55 +0700

    test add

---
+++ src/main/java/com/ioseries/statictest/StatictestApplication.java
@@ -0,0 +1,13 @@
+package com.ioseries.statictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatictestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatictestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ioseries/statictest/StatictestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioseries.statictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatictestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
